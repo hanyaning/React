2ec1d6684f4fc378d8d688b42c7e6d92aee5e7dd
hanyaning <1193985098@qq.com>
2019-10-25 10:16:21 +0800

    messsage

---
+++ build/precache-manifest.f79752289dfa4c6eb52cfd8894f286c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24133a0d198c80539433db63871fa9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d7be9a2cc3b27b989fe",
+    "url": "/static/css/main.8e43f6a1.chunk.css"
+  },
+  {
+    "revision": "b2e90e623b7219c68156",
+    "url": "/static/js/2.50fef47e.chunk.js"
+  },
+  {
+    "revision": "4d7be9a2cc3b27b989fe",
+    "url": "/static/js/main.db96a0df.chunk.js"
+  },
+  {
+    "revision": "ada1bee9e646f835bf9b",
+    "url": "/static/js/runtime-main.8acda0e8.js"
+  }
+]);
